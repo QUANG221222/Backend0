2f57c39f781e90d5bcc82fc28c8dd3e7fc54dcb0
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-09-19 11:02:17 +0700

    More routes

--- readme.md
+++ readme.md
@@ -25,3 +25,9 @@ _cài thư viện express_: npm i --save-exact@"version express"
 Không đẩy node\*modules lên git sử dụng gitignore
 _Running tasks_ : npm run <task-name>
 "$ npm run"
+_More routes_: app.METHOD(PATH, HANDLER)
+_app_: ở đây là ứng dụng express
+_method_: là HTTPrequestmethod, viếtthường(sẽ đề cập tới sau)
+_methodGET_: sẽ nói với Express cần trả ra nội dung cho client
+_Path_: đường link(route) trên server
+_Handler_: function để xử lý khi route được match
--- sever.js
+++ sever.js
@@ -1,10 +1,17 @@
-const express = require("express");
-// import express from "express"; error
-const app = express();
-const port = 8080;
+const express = require("express"); //common js
+// import express from "express"; error //es modules
+const app = express(); //app express
+const port = 8080; //port
 
+//Khai báo route
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/abc", (req, res) => {
+  res.send("Check ABC");
+});
+app.get("/hoidanit", (req, res) => {
+  res.send("<h1> Nguyen Nhat Quang </h1>");
 });
 
 app.listen(port, () => {
