eb7b91b4f758c778c78af8fddac492bb5f6bd4b7
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-10-21 14:36:58 +0700

    #39 Design List User

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,11 +4,13 @@ const {
   getABC,
   getHoiDanIT,
   postCreateUser,
+  getCreatePage,
 } = require("../controllers/homeController");
 const router = express.Router();
 // router.METHOD("/route", handler)
 router.get("/", getHomepage);
 router.get("/abc", getABC);
 router.get("/hoidanit", getHoiDanIT);
+router.get("/create", getCreatePage);
 router.post("/create-user", postCreateUser);
 module.exports = router; //export default
